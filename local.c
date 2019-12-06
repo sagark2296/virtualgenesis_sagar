d1c9a4c6ecdc9b75e07fbec692d563b500ba6467
sagark2296 <30574839+sagark2296@users.noreply.github.com>
2019-12-06 10:55:11 +0530

    Create local.c

---
+++ local.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void test(void);
+int x=100,g=200;
+int main() 
+{
+  int x=300,y=400;
+  ++x;
+  ++g;
+  printf("x=%d,y=%d,g=%d\n",x,y,g);
+  test();
+  printf("g=%d\n",g);
+  return 0;
+}
+void test()
+{
+ int z=500;
+ printf("x=%d,z=%d,g=%d\n",x,z);
+ g=600;
+}
