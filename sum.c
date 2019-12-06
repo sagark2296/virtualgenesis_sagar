f3c1a64bd80b5311c82486ac0c73df4fdfb85c91
sagark2296 <30574839+sagark2296@users.noreply.github.com>
2019-12-06 10:52:34 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int sum(int,int);
+int main() {
+  int a,b,c,d;
+  a=10;
+  b=20;
+  c=sum(a,b);
+  printf("sum is %d\n",c);
+  d=sum(12,15);
+  printf("sum is %d\n",d);
+  printf("sum is %d\n",sum(18,16));
+  return 0;
+}
+int sum(int x,int y)
+{
+  int z;
+  z=x+y;
+  return z;
+}
